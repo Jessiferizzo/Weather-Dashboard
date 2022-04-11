8c40a82182efab55bedd98508feaada53f785e07
Zoey <jsisavath@gmail.com>
2022-04-10 22:12:14 -0500

    added READme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
+//const variables//
+
+
+const APIkey ="7a57fbcfbd1ac395abbe46219d50d833";
 
 
 
+//city search in input city form shows CURRENT weather conditions//
+//CURRENT city div shows city name, date, temp, wind, humidity, UV index//
 
-/*use thisHistory array, populate with local storage history if present
+
+//UV index changes colors to show favorable, moderate, severe//
+
+
+//city search in input form shows FUTURE weather conditions in 5-day forecast//
+//FUTURE forecast displays date, icon representative of weather,temperate, wind speed, and humidity// 
+
+
+//city search in input form is saved in "search history" using JSON storage//
+
+
+//clicking on city in SEARCH HISTORY displays all content again for that city//
+
+
+
+/*save to local storage - use thisHistory array, populate with local storage history if present
 if (JSON.parse(localStorage.getItem("historyArray"))) {
     var thisHistory = JSON.parse(localStorage.getItem("historyArray"));
   } else {
